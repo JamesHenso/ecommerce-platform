c43d3ed26f88a958e137f445260077cf90b2719f
Nguyen Huu Khanh <sieunhangao09123@gmail.com>
2026-09-08 17:50:28 +0700

    feat: updating use catalog API

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import { authRouter } from "./modules/auth/auth.routes.js";
+import { catalogRoutes } from "./modules/catalog/catalog.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -8,6 +9,7 @@ const port = Number(process.env.PORT ?? 3000);
 
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use("/api/catalog", catalogRoutes);
 app.use(errorHandler);
 
 app.listen(port, () => {
