34748e1fde2204df611acb8749d7988a41e17723
Nguyen Huu Khanh <sieunhangao09123@gmail.com>
2026-09-09 16:23:48 +0700

    feat: new api cart

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import { authRouter } from "./modules/auth/auth.routes.js";
 import { catalogRoutes } from "./modules/catalog/catalog.routes.js";
+import { cartRoutes } from "./modules/cart/cart.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -10,6 +11,7 @@ const port = Number(process.env.PORT ?? 3000);
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/catalog", catalogRoutes);
+app.use("/api/cart", cartRoutes);
 app.use(errorHandler);
 
 app.listen(port, () => {
