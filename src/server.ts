6cf0e84f110a21b021f46b7c257c1e5bd9a9eb0e
Nguyen Huu Khanh <sieunhangao09123@gmail.com>
2026-09-11 18:24:18 +0700

    chore: api order and payment

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,27 @@
 import "dotenv/config";
 import express from "express";
 import { authRouter } from "./modules/auth/auth.routes.js";
-import { catalogRoutes } from "./modules/catalog/catalog.routes.js";
-import { cartRoutes } from "./modules/cart/cart.routes.js";
+import { catalogRouter } from "./modules/catalog/catalog.routes.js";
+import { cartRouter } from "./modules/cart/cart.routes.js";
+import { orderRouter } from "./modules/order/order.routes.js";
+import { paymentRouter } from "./modules/payment/payment.routes.js";
+import * as paymentController from "./modules/payment/payment.controller.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 const port = Number(process.env.PORT ?? 3000);
 
+app.post(
+  "/api/payment/webhook",
+  express.raw({ type: "application/json" }),
+  paymentController.handleStripeWebHook
+);
 app.use(express.json());
 app.use("/api/auth", authRouter);
-app.use("/api/catalog", catalogRoutes);
-app.use("/api/cart", cartRoutes);
+app.use("/api/catalog", catalogRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter)
+app.use("/api/payment", paymentRouter);
 app.use(errorHandler);
 
 app.listen(port, () => {
